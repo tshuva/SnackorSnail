a78013863ea8ad64cb5bd7656b2fb04f1cb96707
Miki Meerson <mikimeerson44@gmail.com>
2020-09-05 20:04:35 +0300

    Deleted class files and created create_list method

--- Node.py
+++
@@ -1,6 +0,0 @@
-# A single node of a singly linked list
-class Node:
-  def __init__(self, data = None, next=None): 
-    self.data = data
-    self.next = next
-
--- app.py
+++ app.py
@@ -1,26 +1,94 @@
-from List import List
-from Node import Node
 import random
 
-list = List()
 
-opt=["snake", "snail"]
-listType = random.choices(opt,cum_weights = [0.5,1.0])
+# A single node of a singly linked list
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next = next_node
 
-if (listType == opt[0] or True): 
-  nextNode = random.choices([None,random.randrange(50)],cum_weights = [0.01,1.0])
-  while(nextNode[0] is not None) : 
-    list.insert(nextNode[0])
-    nextNode = random.choices([None,random.randrange(50)],cum_weights = [0.01,1.0])
-else :
- while(Continue) :
-  list.insert(random.randrange(50))
-  Continue = random.choices([False,True],cum_weights = [0.15,1.0]) 
- while(not last) :
-  last = random.choices([True,False],cum_weights = [0.15,1.0])
 
-list.printList()
-# SnackorSnail(L)
+class List:
+    """ A Linked List class with a single head node """
 
-# def SnackorSnail(L):
+    def __init__(self, head=None):
+        if head:
+            self.head = head
+        else:
+            self.head = None
 
+    def insert(self, data):
+        """ insert to end of linked list """
+        new_node = Node(data)
+        if self.head:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
+
+    def print_snake(self):
+        """ print method for the snake list """
+
+        print("SNAKE LIST:")
+
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
+    def print_snail(self, start_of_loop):
+        """ print method for the snail list """
+
+        print("SNAIL LIST:")
+        current = self.head
+        reached_loop = False
+
+        # Go through list until reaching start of loop for the second time
+        while not (current == start_of_loop and reached_loop):
+            print(current.data)
+            if current == start_of_loop:
+                reached_loop = True
+                print("Start of loop above")
+            current = current.next
+
+        print(current.data)  # Print start of loop again to close it
+
+
+def create_list():
+    list = List(head=Node(random.randint(1, 100)))
+    is_snake = random.randint(0, 1)  # Snake - 1, Snail - 0
+
+    if is_snake:  # Snake list
+        while random.randint(1, 100) != 1:  # Should create another node
+            list.insert(data=random.randint(1, 100))
+        list.print_snake()
+
+    else:  # Snail list
+        start_of_loop = None
+        curr_p = list.head
+
+        while random.randint(1, 100) > 2:  # Should create another node
+            list.insert(data=random.randint(1, 100))
+            curr_p = curr_p.next  # Point to last node
+
+            # Set start of loop if not set yet
+            if random.randint(1, 1000) <= 15 and start_of_loop is None:  # Is this the start of loop
+                start_of_loop = curr_p
+
+        if start_of_loop is None:  # Set random start of loop if necessary
+            start_of_loop = list.head
+            temp_p = list.head
+
+            while temp_p and start_of_loop == list.head:
+                if random.randint(0, 1):
+                    start_of_loop = temp_p
+                temp_p = temp_p.next
+
+        curr_p.next = start_of_loop  # Close loop
+        list.print_snail(start_of_loop)
+
+
+if __name__ == '__main__':
+    create_list()
